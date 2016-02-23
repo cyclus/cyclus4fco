c820eb012eb87cdb2f7d816270b40d36ae3a2aa1
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-02-22 21:21:44 -0600

    first step

--- cyclus2fco.py
+++ cyclus2fco.py
@@ -1,33 +1,55 @@
 #! /usr/bin/env python
 from __future__ import print_function, unicode_literals
-import os
 import sys
-import tarfile
 import subprocess
-import shutil
 import io
+import re
 
 
+def read_input(input):
+  print(input)
 
-def update_describe():
-    root_dir = os.path.split(__file__)[0]
-    fname = os.path.join(root_dir, 'src', 'version.cc.in')
-    cmd = 'touch {0}'.format(fname)
-    subprocess.check_call(cmd.split(), shell=(os.name == 'nt'))
 
+def read( parameters ):
+  print(paramters)
+
+
+#def write_outputfile():
 
 
 def main():
-    output = "echo "
-    prog = 'cyan'
-    db = 'cyclus.sqlite'
-    cmd = prog + ' -db ' + db + ' trans'
-    print(cmd)
+  output = "echo "
+  prog = 'cyan'
+  db = 'cyclus.sqlite'
+  cmd = prog + ' -db ' + db + ' trans'
+  print(cmd)
 
-    output = subprocess.check_output(cmd.split())
+  output = subprocess.check_output(cmd.split())
+  buf = output.decode("utf-8")
+  #  line = buf.readline()
+  # print(line)
+  print(buf.__class__)
+  lines = buf.splitlines()
+  matrix = []
 
-    print(output)
+  print(len(lines))
+  for line in lines:
+    cols = line.split()
+    matrix.append(cols)
+
+  read_input(sys.argv[1])
+#  print(len(matrix))
 
 
 
+#cols = line.split()
+#print(len(cols))
+
+# print(cols.__class__)
+
+#matrix = []
+
+#matrix.append(cols)
+
+# print(matrix)
 main()
