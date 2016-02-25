2e5949d1d74e9aa5ddfcb39de17c5d816e004513
MOUGINOT B <mouginot.baptiste@gmail.com>
2016-02-24 21:01:25 -0600

    add input exemple, start reading it

--- cyclus2fco.py
+++ cyclus2fco.py
@@ -10,7 +10,7 @@ def read_input(input):
   f = open(input, 'r')
   matrix = []
   for line in f:
-    print(line)
+    #    print(line)
     matrix.append(line)
 
   return matrix
@@ -19,7 +19,7 @@ def read( parameters ):
   print(paramters)
 
 
-def recover_info( cmd ):
+def cyan( cmd ):
   output = subprocess.check_output(cmd.split())
   buf = output.decode("utf-8")
   lines = buf.splitlines()
@@ -29,13 +29,27 @@ def recover_info( cmd ):
     matrix.append(cols)
   return matrix
 
-def treat_the_matrix( matrix)
+def treat_the_matrix(matrix):
+  print(matrix)
 
 
+def build_cmd(inputline):
+  print(inputline)
 
-def build_cmd(inputline)
 
-
+def recover_info(line):
+  if len(line.split(' ',1)) != 2:
+          print("badline", line)
+  else :
+    line_hd,line_def = line.split(' ',1)
+    hd_id = line_hd[0:2]
+    if hd_id == 'RE' : print("reactor")
+    elif hd_id == 'RP' : print("repro")
+    elif hd_id == 'CO' : print("cooling")
+    elif hd_id == 'WR' : print("waiting repro")
+    elif hd_id == 'ST' : print("storage")
+    elif hd_id == 'WT' : print("waste")
+    else : print("bad keyword in: ", line )
 #def write_outputfile():
 
 
@@ -43,16 +57,21 @@ def main():
 
   if len(sys.argv) != 2 :
     print("missing argument !!")
-  #   quit()
+    quit()
+
+  input_list = read_input(sys.argv[1])
 
 
+  for line in input_list:
+    if line != '\n':
+      recover_info(line)
 
-  prog = 'cyan'
-  db = 'cyclus.sqlite'
-  cmd = prog + ' -db ' + db + ' trans'
-  matrix = recover_info(cmd)
+#  prog = 'cyan'
+#  db = 'cyclus.sqlite'
+#  cmd = prog + ' -db ' + db + ' trans'
+# matrix = recover_info(cmd)
 
-  read_input(sys.argv[1])
+#  read_input(sys.argv[1])
 #  print(len(matrix))
 
 
